ba2a807a468c31dc8bfec2e3fa8f546c20c5a1bf
DankAvHi <thexcat86@gmail.com>
2024-04-26 15:35:06 +0300

    add graph

--- src/pages/TablePage/TablePage.tsx
+++ src/pages/TablePage/TablePage.tsx
@@ -10,6 +10,32 @@ const modelTypeOptions = [
     { label: "ARIMA", value: "SARIMA(X)" },
 ];
 
+const pred = {
+    "2023-08-01": 25.6,
+
+    "2023-08-02": 28.1,
+
+    "2023-08-03": 26.3,
+
+    "2023-08-04": 29.7,
+
+    "2023-08-05": 27.9,
+
+    "2023-08-06": 30.2,
+
+    "2023-08-07": 28.8,
+
+    "2023-08-08": 31.5,
+
+    "2023-08-09": 32.1,
+
+    "2023-08-10": 30.7,
+
+    "2023-08-11": 33.4,
+
+    "2023-08-12": 31.9,
+};
+
 export const TablePage = () => {
     return (
         <div className={`${styles.TablePage}`}>
@@ -40,7 +66,7 @@ export const TablePage = () => {
             </div>
             <div className={styles.result}>
                 <Typography align="center" size="titleSmall" color="black">{`Результат прогноза `}</Typography>
-                <Graph prediction={{} as { [key: string]: number }} />
+                <Graph prediction={pred} />
                 <Button size="small" type="secondary">{`Скачать результат`}</Button>
             </div>
         </div>
--- src/widgets/Graph/Graph.tsx
+++ src/widgets/Graph/Graph.tsx
@@ -1,15 +1,38 @@
 import styles from "./Graph.module.css";
+import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area } from "recharts";
 
 type GraphProps = {
     prediction: { [key: string]: number };
 };
 
 export const Graph: React.FC<GraphProps> = ({ prediction }) => {
+    const data = Object.keys(prediction).map((pred) => {
+        const value = prediction[pred];
+
+        const name = new Date(pred).getDate();
+        return {
+            name: name,
+            uv: value,
+        };
+    });
+
     return (
         <div className={styles.Graph}>
-            {Object.keys(prediction).map((pred) => (
-                <div key={pred}>{prediction[pred]}</div>
-            ))}
+            <span className={styles.value}>{`target value`}</span>
+            <AreaChart width={416} height={382} data={data} margin={{ top: 10, right: 10, left: 0, bottom: 0 }}>
+                <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#F1E15B" stopOpacity={0.8} />
+                        <stop offset="95%" stopColor="#F1E15B" stopOpacity={0} />
+                    </linearGradient>
+                </defs>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Area type="monotone" dataKey="uv" stroke="#F1E15B" fillOpacity={1} fill="url(#colorUv)" />
+            </AreaChart>
+            <span className={styles.date}>{`date`}</span>
         </div>
     );
 };
