b2d5db900c87dd9c7adb3d106345cc1eb944d0fb
DankAvHi <thexcat86@gmail.com>
2024-04-30 17:30:13 +0300

    some changes

--- src/pages/TablePage/TablePage.tsx
+++ src/pages/TablePage/TablePage.tsx
@@ -4,39 +4,85 @@ import { Button } from "../../shared/UI/Button";
 import { Select } from "../../shared/UI/Select";
 import { Input } from "../../shared/UI/Input";
 import { Graph } from "../../widgets/Graph";
+import { useFileContext } from "../../app/providers";
+import { apiClient } from "../../shared/api";
+import { useEffect, useState } from "react";
+import { SelectOption } from "../../shared/types.d";
+import { PredictionTimeUnit } from "../../shared/api/generated";
 
 const modelTypeOptions = [
     { label: "Линейная Регрессия", value: "LR" },
     { label: "ARIMA", value: "SARIMA(X)" },
 ];
 
-const pred = {
-    "2023-08-01": 25.6,
-
-    "2023-08-02": 28.1,
-
-    "2023-08-03": 26.3,
-
-    "2023-08-04": 29.7,
-
-    "2023-08-05": 27.9,
-
-    "2023-08-06": 30.2,
-
-    "2023-08-07": 28.8,
-
-    "2023-08-08": 31.5,
-
-    "2023-08-09": 32.1,
-
-    "2023-08-10": 30.7,
-
-    "2023-08-11": 33.4,
-
-    "2023-08-12": 31.9,
-};
-
 export const TablePage = () => {
+    const { fileId } = useFileContext();
+    const [pred, setPred] = useState<{ [key: string]: number }>();
+    const [intervalOptions, setIntervalOptions] = useState<{ value: string; label: string }[]>([]);
+    const [predInterval, setPredInterval] = useState<SelectOption>();
+    const [steps, setSteps] = useState<number>();
+    const [modelType, setModelType] = useState<{ label: string; value: string }>();
+
+    useEffect(() => {
+        const fetchInfo = async () => {
+            const pred = await apiClient.postAPIMakePredictions(fileId as number).then(async (res) => res.result);
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            //@ts-ignore
+            setPred(pred);
+            const info = await apiClient.getAPIOneRecord(fileId as number);
+            const value = info.timeUnit;
+            setIntervalOptions(
+                value === "D"
+                    ? [
+                          { value: "D", label: "День" },
+                          { value: "W", label: "Неделя" },
+                          { value: "M", label: "Месяц" },
+                          { value: "Y", label: "Год" },
+                      ]
+                    : value === "W"
+                    ? [
+                          { value: "W", label: "Неделя" },
+                          { value: "M", label: "Месяц" },
+                          { value: "Y", label: "Год" },
+                      ]
+                    : value === "M"
+                    ? [
+                          { value: "M", label: "Месяц" },
+                          { value: "Y", label: "Год" },
+                      ]
+                    : [{ value: "Y", label: "Год" }],
+            );
+        };
+
+        fetchInfo();
+    }, [fileId, pred]);
+
+    const timeUnitSelectOnChangeHandler = (newValue: unknown) => {
+        const time = newValue as SelectOption;
+        setPredInterval(time);
+    };
+    const modelTypeSelectOnChangeHandler = (newValue: unknown) => {
+        const modelType = newValue as SelectOption;
+        setModelType(modelType);
+    };
+
+    const stepsInutOnChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSteps(+event.target.value);
+    };
+
+    const regeneratePred = async () => {
+        const newPred = await apiClient
+            .postAPIMakePredictions(fileId as number, {
+                predictionTimeUnit: PredictionTimeUnit[predInterval!.value],
+                steps: steps as number,
+                predictionType: modelType!.value,
+            })
+            .then(async (res) => res.result);
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        setPred(newPred);
+    };
+
     return (
         <div className={`${styles.TablePage}`}>
             <div className={styles.settings}>
@@ -51,22 +97,26 @@ export const TablePage = () => {
                 <div className={styles.settingsOptions}>
                     <div className={styles.option}>
                         <Typography color="gray">{`Тип модели*`}</Typography>
-                        <Select options={modelTypeOptions} />
+                        <Select onChange={modelTypeSelectOnChangeHandler} options={modelTypeOptions} />
                     </div>
                     <div className={styles.option}>
                         <Typography color="gray">{`Количество шагов прогнозирования* `}</Typography>
-                        <Input placeholder="Введите количество шагов" type="number" />
+                        <Input
+                            onChange={stepsInutOnChangeHandler}
+                            placeholder="Введите количество шагов"
+                            type="number"
+                        />
                     </div>
                     <div className={styles.option}>
                         <Typography color="gray">{`Интервал прогнозирования*`}</Typography>
-                        <Select options={[]} />
+                        <Select onChange={timeUnitSelectOnChangeHandler} options={intervalOptions} />
                     </div>
                 </div>
-                <Button size="small">{`Подтвердить`}</Button>
+                <Button onClick={regeneratePred} size="small">{`Подтвердить`}</Button>
             </div>
             <div className={styles.result}>
                 <Typography align="center" size="titleSmall" color="black">{`Результат прогноза `}</Typography>
-                <Graph prediction={pred} />
+                <Graph prediction={pred || {}} />
                 <Button size="small" type="secondary">{`Скачать результат`}</Button>
             </div>
         </div>
